97a456aabe3f081c9bbd8387d994d16e74f27cbb
burakpadr <b.padir99@gmail.com>
2022-10-03 20:18:42 +0300

    Refactored refreshAddressCache function

--- src/main/java/com/padr/buynow/domain/address/service/AddressScheduledJobService.java
+++ src/main/java/com/padr/buynow/domain/address/service/AddressScheduledJobService.java
@@ -26,35 +26,13 @@ public class AddressScheduledJobService {
     public void refreshAddressCache() {
         deleteOldAddressCache();
 
-        List<Country> countries = countryStateCityClient.getCountries().parallelStream().map(CountryResponse::to)
-                .collect(Collectors.toList());
-
-        countryService.saveAll(countries);
+        List<Country> countries = saveCountriesToCacheAndGet();
 
         countries.parallelStream().forEach(country -> {
-            List<State> states = countryStateCityClient.getStatesByCountryIso(country.getIso()).parallelStream()
-                    .map(stateResponse -> {
-                        State state = stateResponse.to();
-
-                        state.setCountryId(country.getId());
-
-                        return state;
-                    }).collect(Collectors.toList());
-
-            stateService.saveAll(states);
+            List<State> states = saveStatesToCacheAndGet(country);
 
             states.parallelStream().forEach(state -> {
-                List<City> cities = countryStateCityClient
-                        .getCitiesByCountryIsoAndStateIso(country.getIso(), state.getIso()).parallelStream()
-                        .map(cityResponse -> {
-                            City city = cityResponse.to();
-
-                            city.setStateId(state.getId());
-
-                            return city;
-                        }).collect(Collectors.toList());
-
-                cityService.saveAll(cities);
+                saveCitiesToCache(country, state);
             });
         });
     }
@@ -63,5 +41,43 @@ public class AddressScheduledJobService {
         countryService.deleteAll();
         stateService.deleteAll();
         cityService.deleteAll();
+    }
+
+    private List<Country> saveCountriesToCacheAndGet() {
+        List<Country> countries = countryStateCityClient.getCountries().parallelStream().map(CountryResponse::to)
+                .collect(Collectors.toList());
+
+        countryService.saveAll(countries);
+
+        return countries;
+    }
+
+    private List<State> saveStatesToCacheAndGet(Country country) {
+        List<State> states = countryStateCityClient.getStatesByCountryIso(country.getIso()).parallelStream()
+                .map(stateResponse -> {
+                    State state = stateResponse.to();
+
+                    state.setCountryId(country.getId());
+
+                    return state;
+                }).collect(Collectors.toList());
+
+        stateService.saveAll(states);
+
+        return states;
+    }
+
+    private void saveCitiesToCache(Country country, State state) {
+        List<City> cities = countryStateCityClient
+                .getCitiesByCountryIsoAndStateIso(country.getIso(), state.getIso()).parallelStream()
+                .map(cityResponse -> {
+                    City city = cityResponse.to();
+
+                    city.setStateId(state.getId());
+
+                    return city;
+                }).collect(Collectors.toList());
+
+        cityService.saveAll(cities);
     }
 }
