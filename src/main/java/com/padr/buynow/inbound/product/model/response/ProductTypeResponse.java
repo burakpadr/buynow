b6fab13c5df764665cab31e2cf7dbf157a58d826
burakpadr <b.padir99@gmail.com>
2022-10-16 00:44:11 +0300

    Added new field to the product type response

--- src/main/java/com/padr/buynow/inbound/product/model/response/ProductTypeResponse.java
+++ src/main/java/com/padr/buynow/inbound/product/model/response/ProductTypeResponse.java
@@ -1,5 +1,8 @@
 package com.padr.buynow.inbound.product.model.response;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.padr.buynow.domain.product.entity.ProductType;
 
 import lombok.Builder;
@@ -12,12 +15,17 @@ public class ProductTypeResponse {
     private Long id;
     private String name;
     private String label;
+    private Boolean isActive;
+    private List<ProductTypeAttributeGroupResponse> productTypeAttributeGroups;
 
     public static ProductTypeResponse of(ProductType productType) {
         return ProductTypeResponse.builder()
                 .id(productType.getId())
                 .name(productType.getName())
                 .label(productType.getLabel())
+                .isActive(productType.getIsActive())
+                .productTypeAttributeGroups(productType.getProductTypeAttributeGroups().stream()
+                        .map(ProductTypeAttributeGroupResponse::of).collect(Collectors.toList()))
                 .build();
     }
 }
