17724cc4ed2d85339bb9a5895803633a59e7b379
burakpadr <b.padir99@gmail.com>
2022-11-23 23:46:26 +0300

    Created auction notice and auction notice cancellation entities

---
+++ src/main/java/com/padr/buynow/domain/notice/constant/AuctionNoticeStep.java
@@ -0,0 +1,10 @@
+package com.padr.buynow.domain.notice.constant;
+
+public enum AuctionNoticeStep {
+    
+    WAITING_FOR_BID,
+    IN_NOTICE,
+    WAITING_FOR_PAYMENT,
+    SOLD,
+    CANCELLED
+}
